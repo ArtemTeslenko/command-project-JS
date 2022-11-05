e3f4dcaf2fa9fcfb8b81ef44c27f058d93159055
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-11-05 16:52:56 +0200

    markupBtnModalFilm

--- src/js/markup/markupBtnModalFilm.js
+++ src/js/markup/markupBtnModalFilm.js
@@ -1,4 +1,5 @@
 export function markupBtnModalFilm() {
+<<<<<<< Updated upstream
   return `<img class="film-card__img"
  src=""
  alt=""
@@ -16,3 +17,23 @@ export function markupBtnModalFilm() {
  </p>
  <button class="button">add to Watched</button>
  <button class="button">add to queue</button>`}
+=======
+              return `<img class="film-card__img"
+             src="https://cdn.pixabay.com/photo/2020/05/26/12/56/image-5222934_960_720.jpg"
+             alt=""
+             id=""
+           />
+             <h2></h2>
+             <ul>
+               <li class="list">Vote/Votes <span></span></li>
+               <li class="list">Popularity <span></span></li>
+               <li class="list">Original Title <span></span></li>
+             </ul>
+             <p>
+               <h3>About</h3>
+               <div></div>
+             </p>
+             <button class="button">add to Watched</button>
+             <button class="button">add to queue</button>`}
+
+>>>>>>> Stashed changes
