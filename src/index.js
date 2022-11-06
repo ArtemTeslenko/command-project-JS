e59c8d4c4b03184299e8cab545972972f9179866
Volodymyr <vlrohmanov@gmail.com>
2022-11-06 15:52:36 +0200

    header lib
    Кнопки пока не сделал, так как модалка на странице Library ложит скрипт

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { renderAll } from './js/common.js';
 
 renderAll();
+
+const btnWatchedEl = document.querySelector('.header-btn__watched');
+console.log(btnWatchedEl);
--- src/js/modal/commonModalOpen.js
+++ src/js/modal/commonModalOpen.js
@@ -1,18 +1,17 @@
-const backdropEl =document.querySelector(".backdrop")
-const buttonTestEl=document.querySelector(".test")
-const buttonClose=document.querySelector(".modal-button")
-
+const backdropEl = document.querySelector('.backdrop');
+const buttonTestEl = document.querySelector('.test');
+const buttonClose = document.querySelector('.modal-button');
 
 function onClouseModal(el) {
-    backdropEl.classList.add("is-hidden")
-  }
+  backdropEl.classList.add('is-hidden');
+}
 
 function onOpenModal(e) {
-    e.preventDefault();
-    backdropEl.classList.remove("is-hidden")
+  e.preventDefault();
+  backdropEl.classList.remove('is-hidden');
 }
 
 export function onClick(e) {
-  buttonTestEl.addEventListener('click', onOpenModal)
-  buttonClose.addEventListener('click', onClouseModal)
+  buttonTestEl.addEventListener('click', onOpenModal);
+  buttonClose.addEventListener('click', onClouseModal);
 }
