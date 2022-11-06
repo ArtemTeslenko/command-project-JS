d77ed31695866d7e8aa38d217bd3e795ace9aef8
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-11-06 15:21:55 +0200

    adds stile to library cards

--- src/js/render/wachedFilmRender.js
+++ src/js/render/wachedFilmRender.js
@@ -5,6 +5,11 @@ const libraryEl = document.querySelector('.library-gallery');
 export function createGalleryList(filmItems) {
   for (i = 0; i < filmItems.length; i += 1) {
     filmItems[i].genre_ids.splice(2, 5, 'Other');
+    filmItems[i].release_date = filmItems[i].release_date
+      .split('')
+      .slice(0, 4)
+      .join('');
+    filmItems[i].vote_average = filmItems[i].vote_average.toFixed(1);
   }
   libraryEl.innerHTML = filmsCard(filmItems);
 }
