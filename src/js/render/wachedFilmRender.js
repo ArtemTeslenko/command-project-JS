9cb8716d57ce29b3222203065d6668ff1db36663
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-11-07 00:44:23 +0200

    change library markup

--- src/js/render/wachedFilmRender.js
+++ src/js/render/wachedFilmRender.js
@@ -1,15 +1,8 @@
-import filmsCard from '../markup/templates/watchedFilmsMarkup.hbs';
-import { getGenreName } from '../customFunction/getGenreName';
+import { createLibraryMarkup } from '../markup/libraryMrkup';
 
 const libraryEl = document.querySelector('.library-gallery');
-export function createGalleryList(filmItems) {
-  for (i = 0; i < filmItems.length; i += 1) {
-    filmItems[i].genre_ids.splice(2, 5, 'Other');
-    filmItems[i].release_date = filmItems[i].release_date
-      .split('')
-      .slice(0, 4)
-      .join('');
-    filmItems[i].vote_average = filmItems[i].vote_average.toFixed(1);
-  }
-  libraryEl.innerHTML = filmsCard(filmItems);
+export function createLibraryList(filmItems) {
+  const libraryMarkup = createLibraryMarkup(filmItems);
+
+  libraryEl.innerHTML = libraryMarkup;
 }
