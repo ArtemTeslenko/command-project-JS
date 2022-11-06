3add7303caef27537149d5558fd3014a3997083c
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-11-06 14:27:28 +0200

    corrects genre

--- src/js/render/library-items.js
+++ src/js/render/library-items.js
@@ -1,24 +1,27 @@
 export const watchedItems = [
   {
+    title: 'All Quiet on the Western Front',
     poster_path:
-      'https://www.themoviedb.org/t/p/w500/t7CxFuwwknEWqPNjzERODfo9EQr.jpg',
-    title: 'Enola Holmes 2',
-    vote_average: '8.036',
-    release_date: '2022',
+      'https://www.themoviedb.org/t/p/w500/2IRjbi9cADuDMKmHdLK7LaqQDKA.jpg',
+    genre_ids: ['Action', 'Drama', 'Adventure', 'Tragedy'],
+    release_date: '2022-10-07',
+    vote_average: 7.944,
   },
   {
+    title: 'The Takeover',
     poster_path:
-      'https://image.tmdb.org/t/p/w500/t7CxFuwwknEWqPNjzERODfo9EQr.jpg',
-    title: 'Enola Holmes 2',
-    vote_average: '8.036',
-    release_date: '2022',
+      'https://www.themoviedb.org/t/p/w500/i6bqxo8o0yYDUUaQtkHaism8Bkr.jpg',
+    genre_ids: ['Action', 'Crime', 'Thriller'],
+    release_date: '2022-11-01',
+    vote_average: 5.828,
   },
   {
+    title: 'All Quiet on the Western Front',
     poster_path:
-      'https://image.tmdb.org/t/p/w500/1seIJ8wIKo3KXp7hnwS5wj69U8M.jpg',
-    title: 'Enola Holmes 2',
-    vote_average: '8.036',
-    release_date: '2022',
+      'https://www.themoviedb.org/t/p/w500/2IRjbi9cADuDMKmHdLK7LaqQDKA.jpg',
+    genre_ids: ['Action', 'Drama', 'Adventure', 'Tragedy'],
+    release_date: '2022-10-07',
+    vote_average: 7.944,
   },
 ];
 
@@ -28,21 +31,21 @@ export const galleryItems = [
       'https://image.tmdb.org/t/p/w500/1seIJ8wIKo3KXp7hnwS5wj69U8M.jpg',
     title: 'Enola Holmes 2',
     vote_average: '8.036',
-    release_date: '2022',
+    release_date: '2022-10-07',
   },
   {
     poster_path:
       'https://image.tmdb.org/t/p/w500/t7CxFuwwknEWqPNjzERODfo9EQr.jpg',
     title: 'Enola Holmes 2',
     vote_average: '8.036',
-    release_date: '2022',
+    release_date: '2022-10-07',
   },
   {
     poster_path:
       'https://image.tmdb.org/t/p/w500/t7CxFuwwknEWqPNjzERODfo9EQr.jpg',
     title: 'Enola Holmes 2',
     vote_average: '8.036',
-    release_date: '2022',
+    release_date: '2022-10-07',
   },
   {
     poster_path:
--- src/js/render/wachedFilmRender.js
+++ src/js/render/wachedFilmRender.js
@@ -1,6 +1,10 @@
 import filmsCard from '../markup/templates/watchedFilmsMarkup.hbs';
+import { getGenreName } from '../customFunction/getGenreName';
 
 const libraryEl = document.querySelector('.library-gallery');
 export function createGalleryList(filmItems) {
+  for (i = 0; i < filmItems.length; i += 1) {
+    filmItems[i].genre_ids.splice(2, 5, 'Other');
+  }
   libraryEl.innerHTML = filmsCard(filmItems);
 }
