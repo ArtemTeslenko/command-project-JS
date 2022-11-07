6d9ac774746634e3a41bcc9d378be122cac8574a
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2022-11-07 13:51:25 +0200

    folders conflict resolving

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { refs } from './js/reference/homeRefs.js';
 import { trendsControls } from './js/controls';
 import { onOpenModalFilm } from './js/modal';
+import { scrollToTop } from './js/controls';
 
 trendsControls();
 onOpenModalFilm();
+scrollToTop();
