eda886fb1f45dc437e19373c90166a3ff7215aae
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-11-08 03:48:58 +0200

    change library

--- src/library.js
+++ src/library.js
@@ -1,9 +1,4 @@
 import { refs } from './js/reference/libraryRefs.js';
-<<<<<<< Updated upstream
 import { onMyLibraryOpen } from './js/controls';
-=======
-import { createLibraryList } from './js/render';
-import { watchedItems } from './js/render/library-items';
->>>>>>> Stashed changes
 
 onMyLibraryOpen();
