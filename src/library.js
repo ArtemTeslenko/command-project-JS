c2b56abfcd7487f0f489a0a4af3167d803ee47c9
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-11-08 03:46:16 +0200

    Update library.js
    Додоє інформацію в локал по кліку на кнопки модалки та рендерить сторінку бібліотеки (поки без переключення між watched  та queue). Додав до кнопок модалки два класи для слухача кліку.

--- src/library.js
+++ src/library.js
@@ -1,4 +1,9 @@
 import { refs } from './js/reference/libraryRefs.js';
+<<<<<<< Updated upstream
 import { onMyLibraryOpen } from './js/controls';
+=======
+import { createLibraryList } from './js/render';
+import { watchedItems } from './js/render/library-items';
+>>>>>>> Stashed changes
 
 onMyLibraryOpen();
