9e7e6fcb03518aff5d131fe4041abd990ff8876c
Богдан Опанасенко <bodia23042005@gmail.com>
2022-11-08 05:16:14 +0200

    css done

--- src/js/markup/registerBtnModal.js
+++ src/js/markup/registerBtnModal.js
@@ -1,16 +1,19 @@
 export function registerBtnModal() {
-  return `
-            <form class="form">
-            <h2 class="title-modal">Authorization</h2>
-            <div class="form-field">
-                <label class="form-label" for="mail">Email</label>
-                <input type="email" class="form-input" name="mail" id="mail" required autocomplete="email">
+  return `<form class="form">
+    <h2 class="title-modal">Authorization</h2>
+    <div class="form-field">
+        <label class="form-label" for="mail">Email</label>
+        <input type="email" class="form-input" name="mail" id="mail" required autocomplete="email" placeholder="
+Something like name@site.com">
 
-            </div>
-            <div class="form-field">
+    </div>
+    <div class="form-field">
 
-                <label class="form-label" for="password">Password</label>
-                <input class="form-input" type="password" name="password" id="password" required autocomplete="on">
-            </div>
-        </form>`;
+        <label class="form-label" for="password">Password</label>
+        <input class="form-input" type="password" name="password" id="password" required autocomplete="on"
+            placeholder="Some kind of complex cipher in your memory...">
+    </div>
+     <button type="button" class="button-reg-modal">Log in</button>
+    
+</form>`;
 }
