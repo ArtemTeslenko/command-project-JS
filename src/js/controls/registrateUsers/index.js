c1163b0977a528662d5b8224613717ef9c533b8c
VladosSid <sidorsv121097@gmail.com>
2022-11-08 17:26:44 +0200

    add REG and AUTH User

--- src/js/controls/registrateUsers/index.js
+++ src/js/controls/registrateUsers/index.js
@@ -1,11 +1,10 @@
 export * from './markupModalReg';
 export * from './openModalAuthUser';
-// export * from './fireBaseIndex';
-// export * from './openModal';
-// export * from './renderMyLibrary';
-// export * from './userAuth';
-// export * from './userInActive';
-// export * from './verifySession';
+export * from './fireBaseIndex';
+export * from './renderMyLibrary';
+export * from './userAuth';
+export * from './userInActive';
+export * from './verifySession';
 
-// import { veryfiUser } from './index';
-// veryfiUser();
+import { veryfiUser } from './index';
+veryfiUser();
