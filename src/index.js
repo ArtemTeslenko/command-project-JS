34698fa5d33b4ddfbb8b98386114382d2fa465e8
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2022-11-09 16:03:44 +0200

    fix root, exclude 'modal'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { refs } from './js/reference/homeRefs';
 import { trendsControls, scrollToTop } from './js/controls';
-import { onOpenModalFilm, modalTemplate } from './js/modal';
+import { onOpenModalFilm, modalTemplate } from './js/controls';
 import {} from './js/controls/registrateUsers';
 import { onListenerSearchMovie } from './js/requests';
 
