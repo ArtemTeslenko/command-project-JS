54f5e56f6e02e73e75bf736b6a2adc707448f777
Volodymyr <vlrohmanov@gmail.com>
2022-11-11 11:27:06 +0200

    add spiner home
    Добавил спинер

--- src/js/requests/fetchSearchFilm.js
+++ src/js/requests/fetchSearchFilm.js
@@ -4,9 +4,13 @@ import { paginationControl } from '../customFunction/paginationControls';
 import { filmsTrendRender } from '../render';
 import { fetchFilms } from './fetchFilmsTrends';
 import { filmsTrendRender } from '../render';
+import {
+  openSpinnerHome,
+  closeSpinnerHome,
+} from '../customFunction/spinerHome';
 
 const keyApi = '894ef72300682f1db325dae2afe3e7e2';
-const textNotifly =
+const textNotify =
   'Search result not successful. Enter the correct movie name and try again';
 let movieName;
 let movieNameSubmit;
@@ -22,14 +26,24 @@ export function onListenerSearchMovie() {
 
 function onSearchSubmitMovie(event) {
   event.preventDefault();
-  refs.galleryEl.innerHTML = '';
+  console.log(event);
+  // refs.galleryEl.innerHTML = '';
   refs.paginationEl.innerHTML = '';
+  refs.paginationSearchEl.innerHTML = '';
   movieName = refs.searchMovieEl[0].value;
   movieNameSubmit = refs.searchMovieEl[1].value;
-  movieNameSubmit = movieName;
-  trendUrl = `https://api.themoviedb.org/3/search/movie?api_key=${keyApi}&query=${movieNameSubmit}&page=`;
 
-  mainSearch();
+  if (refs.searchMovieEl[0].value === '') {
+    return;
+  } else {
+    console.log(movieNameSubmit);
+    console.log(1);
+    movieNameSubmit = movieName;
+    trendUrl = `https://api.themoviedb.org/3/search/movie?api_key=${keyApi}&query=${movieNameSubmit}&page=`;
+    // openSpinnerHome();
+    mainSearch();
+    // window.onload = closeSpinnerHome();
+  }
 }
 
 // -----Функция вызова слушателя кнопок и ссылок пагинации-----
@@ -44,11 +58,13 @@ export function onSearchPagination() {
 // -----Основная функция поиска фильмов и рендера фильмов и кнопок(ссылок) пагинации-----
 
 function mainSearch(page) {
+  openSpinnerHome();
+
   refs.galleryEl.innerHTML = '';
   fetchFilms(page, trendUrl)
     .then(data => {
       if (data.results.length === 0) {
-        Notiflix.Notify.failure(`${textNotifly}`);
+        Notiflix.Notify.failure(`${textNotify}`);
       } else {
         const destinationEl = refs.galleryEl;
         filmsTrendRender(data, destinationEl);
@@ -64,4 +80,5 @@ function mainSearch(page) {
       }
     })
     .catch(err => console.log(err));
+  window.onload = closeSpinnerHome();
 }
