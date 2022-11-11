553f2b82e408a504c887a5e2761415c8f0a8bcd7
GannaKov <zlatta2000@gmail.com>
2022-11-11 19:07:42 +0200

    dangeres

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import { refs } from './js/reference/homeRefs';
 import { trendsControls, scrollToTop } from './js/controls';
 import { onOpenModalFilm, modalTemplate } from './js/controls';
 import {} from './js/controls/registrateUsers';
-import { onListenerSearchMovie, onSearchPagination } from './js/requests';
+//import { onListenerSearchMovie, onSearchPagination } from './js/requests';
 import { showTeam } from './js/controls';
-
+import { onListenerSearchMovie } from './js/requests';
 onListenerSearchMovie();
-onSearchPagination();
+////onSearchPagination();
 trendsControls();
 onOpenModalFilm();
 // зразок функції для відкриття чистої модалки
--- src/js/controls/trendsControl.js
+++ src/js/controls/trendsControl.js
@@ -9,7 +9,36 @@ import { cleanRender } from '../customFunction/functionCleanRender';
 import { paginationControl } from '../customFunction/paginationControls';
 
 export let genreIdArr = [];
+//gggggg
+export function onClickGanna(e) {
+  console.log('in Ganna');
+  e.preventDefault();
+  cleanRender(refs.galleryEl);
+  fetchMovies(e.target.dataset.page);
+}
+function fetchMovies(page) {
+  cleanRender(refs.galleryEl);
+  const trendUrl =
+    'https://api.themoviedb.org/3/trending/movie/week?api_key=894ef72300682f1db325dae2afe3e7e2&page=';
+  fetchFilms(page, trendUrl).then(data => {
+    const destinationEl = refs.galleryEl;
 
+    filmsTrendRender(data, destinationEl);
+    let totalPage = data.total_pages;
+    // ------ V copie
+    if (totalPage > 1) {
+      paginationControl(
+        Number(data.total_pages), // total page
+        Number(data.page), // current page
+        trendUrl, // big part of url);
+        refs.paginationEl,
+        onClickGanna
+      );
+    }
+    // ------ end of V copie
+  });
+}
+//gggggggggg
 export function trendsControls() {
   let page = 1;
   async function mainFunctionCode() {
@@ -21,33 +50,13 @@ export function trendsControls() {
         .catch(error => console.log(error));
       fetchMovies(page);
 
-      function fetchMovies(page) {
-        cleanRender(refs.galleryEl);
-        const trendUrl =
-          'https://api.themoviedb.org/3/trending/movie/week?api_key=894ef72300682f1db325dae2afe3e7e2&page=';
-        fetchFilms(page, trendUrl).then(data => {
-          const destinationEl = refs.galleryEl;
-
-          filmsTrendRender(data, destinationEl);
-          let totalPage = data.total_pages;
-          // ------ V copie
-          if (totalPage > 1) {
-            paginationControl(
-              Number(data.total_pages), // total page
-              Number(data.page), // current page
-              trendUrl, // big part of url);
-              refs.paginationEl
-            );
-          }
-          // ------ end of V copie
-        });
-      }
       //----------
-      refs.paginationEl.addEventListener('click', e => {
-        e.preventDefault();
-        //cleanRender(refs.galleryEl);
-        fetchMovies(e.target.dataset.page);
-      });
+
+      // refs.paginationEl.addEventListener('click', e => {
+      //   e.preventDefault();
+      //   //cleanRender(refs.galleryEl);
+      //   fetchMovies(e.target.dataset.page);
+      // });
     } catch (e) {
       console.log(e);
     }
