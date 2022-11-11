8b9eefaa3234f54f0d99e9469a1dc548aa239a73
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-11-11 21:07:37 +0200

    corrects library render

--- src/js/controls/localStorage.js
+++ src/js/controls/localStorage.js
@@ -1,5 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { refs } from '../reference/homeRefs.js';
+import { refs } from '../reference/libraryRefs';
 import { renderLibraryList } from '../render';
 import { cleanRender } from '../customFunction';
 
@@ -55,7 +56,7 @@ export function onWatchedOpen() {
     const watchedItems = JSON.parse(localStorage.getItem(STORAGE_WATCHED_KEY));
     renderLibraryList(watchedItems);
   } catch (error) {
-    cleanRender(libraryEl);
+    cleanRender(refs.libraryEl);
     Notify.warning(warningNotify, {
       position: 'center-top',
     });
@@ -67,7 +68,7 @@ export function onQueueOpen() {
     const qeueItems = JSON.parse(localStorage.getItem(STORAGE_QUEUE_KEY));
     renderLibraryList(qeueItems);
   } catch (error) {
-    cleanRender(libraryEl);
+    cleanRender(refs.libraryEl);
     Notify.warning(warningNotify, {
       position: 'center-top',
     });
