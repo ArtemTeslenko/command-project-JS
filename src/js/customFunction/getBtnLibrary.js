3ef21a442796c01491e4db83b37067aba69790a3
Volodymyr <vlrohmanov@gmail.com>
2022-11-13 08:04:04 +0200

    remove EL and event
    Заменил addEventListener в файле library.js на функцию, убрал ненужные эвенты там-же. Снял is-hidden с ссылки Library - все работает, но лучше проверить с регистрацией. Это функция, которая меняет цвета на кнопках.

--- src/js/customFunction/getBtnLibrary.js
+++ src/js/customFunction/getBtnLibrary.js
@@ -1,14 +1,19 @@
 import { refs } from '../../js/reference/libraryRefs';
 import { onWatchedOpen, onQueueOpen } from '../controls';
 
-export function onQueueBtn(event) {
+export function onClickQueueAndWatchedOpen() {
+  refs.queueEl.addEventListener('click', onQueueBtn);
+  refs.watchedEl.addEventListener('click', onWatchedBtn);
+}
+
+export function onQueueBtn() {
   refs.watchedEl.classList.remove('is-active__btn');
   refs.queueEl.classList.add('is-active__btn');
 
   onQueueOpen();
 }
 
-export function onWatchedBtn(event) {
+export function onWatchedBtn() {
   refs.watchedEl.classList.add('is-active__btn');
   refs.queueEl.classList.remove('is-active__btn');
 
--- src/library.js
+++ src/library.js
@@ -1,10 +1,7 @@
 import { refs } from './js/reference/libraryRefs';
-import { onQueueBtn } from './js/customFunction/getBtnLibrary.js';
-import { onWatchedBtn } from './js/customFunction/getBtnLibrary.js';
+import { onClickQueueAndWatchedOpen } from './js/customFunction/getBtnLibrary.js';
 import { onOpenModalFilm, onWatchedOpen } from './js/controls';
 
-refs.queueEl.addEventListener('click', onQueueBtn);
-refs.watchedEl.addEventListener('click', onWatchedBtn);
-
+onClickQueueAndWatchedOpen();
 onWatchedOpen();
 onOpenModalFilm(refs.libraryEl);
