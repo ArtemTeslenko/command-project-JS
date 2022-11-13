4c7c91a67765ccc01c52f83c0da168660d390bf9
GannaKov <zlatta2000@gmail.com>
2022-11-13 02:56:45 +0200

    вот как то так

--- src/js/controls/showTeamControl.js
+++ src/js/controls/showTeamControl.js
@@ -1,7 +1,30 @@
-import member from "../../footerTeam.json";
-import { renderTeam } from "../render";
-
+import memberA from '../../footerTeam.json';
+import { renderTeam } from '../render';
 
 export function showTeam() {
-    renderTeam(member)
+  const aaaJSON = JSON.stringify(team);
+  console.log('team', team);
+  console.log('json', aaaJSON);
+  const member = JSON.parse(aaaJSON);
+  console.log(member);
+  renderTeam(member);
 }
+const team = [
+  {
+    photo_face: require('../../images/movie_time_22.jpg'),
+    full_name: 'Anna Lazarenko',
+    role: 'Developer/Scram master',
+    git_page: 'https://github.com/AniaLaz',
+    telegram: 'https://t.me/AniaLaz',
+    id: 4,
+  },
+
+  {
+    photo_face: require('../../images/movie_time_22.jpg'),
+    full_name: 'Ganna Kovchyk',
+    role: 'Developer/',
+    git_page: 'https://github.com/GannaKov',
+    telegram: 'https://t.me/Ganna_GannochKa',
+    id: 5,
+  },
+];
