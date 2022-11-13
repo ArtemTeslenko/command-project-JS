744e7159db1b29ca325a893df1aaf16071b7816e
GannaKov <zlatta2000@gmail.com>
2022-11-13 13:38:23 +0200

    clean code

--- src/js/controls/trendsControl.js
+++ src/js/controls/trendsControl.js
@@ -66,25 +66,3 @@ export function trendsControls() {
 
   mainFunctionCode();
 }
-// function fetchMovies(page) {
-//   openSpinnerHome();
-//   cleanRender(refs.galleryEl);
-//   const trendUrl =
-//     'https://api.themoviedb.org/3/trending/movie/week?api_key=894ef72300682f1db325dae2afe3e7e2&page=';
-//   fetchFilms(page, trendUrl).then(data => {
-//     const destinationEl = refs.galleryEl;
-//     filmsTrendRender(data, destinationEl);
-//     closeSpinnerHome();
-//     let totalPage = data.total_pages;
-//     // ------ V copie
-//     if (totalPage > 1) {
-//       paginationControl(
-//         Number(data.total_pages), // total page
-//         Number(data.page), // current page
-//         trendUrl, // big part of url);
-//         onClickTrend
-//       );
-//     }
-//     // ------ end of V copie
-//   });
-// }
