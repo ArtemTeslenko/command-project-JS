536862fcb5533a09c45c8947cb690ba33bc4c006
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-11-13 10:18:20 +0200

    modal watched queue

--- src/library.js
+++ src/library.js
@@ -1,7 +1,11 @@
 import { refs } from './js/reference/libraryRefs';
 import { onQueueBtn } from './js/customFunction/getBtnLibrary.js';
 import { onWatchedBtn } from './js/customFunction/getBtnLibrary.js';
-import { onOpenModalFilm, onWatchedOpen } from './js/controls';
+import {
+  onOpenModalFilm,
+  onOpenModalFilmWatched,
+  onWatchedOpen,
+} from './js/controls';
 
 refs.queueEl.addEventListener('click', onQueueBtn);
 refs.watchedEl.addEventListener('click', onWatchedBtn);
