63ff445c95bf627eb7a7eea820ab3833fb399d04
GannaKov <zlatta2000@gmail.com>
2022-11-16 19:17:45 +0200

    current year

--- src/js/customFunction/filter.js
+++ src/js/customFunction/filter.js
@@ -12,10 +12,16 @@
 //   refs.menu.classList.toggle('is-hidden');
 //   //   refs.body.classList.toggle('no-scroll');
 // }
+const slides = document.querySelectorAll('.filter__input');
+const date = new Date();
+const currentYear = date.getFullYear();
+console.log(currentYear);
+
+// --Принимает данные с инпута i запис в спан выбранный год
 function getVals() {
   // Get slider values
   //var parent = this.parentNode;
-  const slides = document.querySelectorAll('.filter__input');
+  //const slides = document.querySelectorAll('.filter__input');
   console.log('slides', slides);
   const slide1 = parseFloat(slides[0].value);
   const slide2 = parseFloat(slides[1].value);
@@ -29,15 +35,35 @@ function getVals() {
   const displayRangeValues = document.querySelector('.rangeValues'); //[0];
   displayRangeValues.textContent = `${slide1} - ${slide2}`;
 }
-//object.addEventListener("load", myScript);
+//-------
+// --- слушатель
+window.onload = function () {
+  // Initialize Sliders
+  const sliderSections = document.getElementsByClassName('input-year');
+
+  let sliders = document.querySelectorAll('.filter__input'); //
+  console.log(sliders);
+  for (var y = 0; y < sliders.length; y++) {
+    if (sliders[y].type === 'range') {
+      sliders[y].max = currentYear;
+      sliders[y].oninput = getVals;
+      // Manually trigger event first time to display values
+      sliders[y].oninput();
+    }
+  }
+};
+// --------
+
+//-----------------------------------------
+////object.addEventListener("load", myScript);
 
 // window.onload = function () {
 //   // Initialize Sliders
 //   const sliderSections = document.getElementsByClassName('input-year');
-//   console.log(sliderSections);
+
 //   for (var x = 0; x < sliderSections.length; x++) {
-//     console.log(sliderSections[x]);
-//     let sliders = sliderSections[x].getElementsByTagName('input'); //
+//     //     let sliders = sliderSections[x].getElementsByTagName('input'); //
+//     let sliders = sliderSections[x].querySelectorAll('.filter__input');
 //     for (var y = 0; y < sliders.length; y++) {
 //       if (sliders[y].type === 'range') {
 //         sliders[y].oninput = getVals;
@@ -47,17 +73,3 @@ function getVals() {
 //     }
 //   }
 // };
-window.onload = function () {
-  // Initialize Sliders
-  const sliderSections = document.getElementsByClassName('input-year');
-  console.log(sliderSections);
-
-  let sliders = document.querySelectorAll('.filter__input'); //
-  for (var y = 0; y < sliders.length; y++) {
-    if (sliders[y].type === 'range') {
-      sliders[y].oninput = getVals;
-      // Manually trigger event first time to display values
-      sliders[y].oninput();
-    }
-  }
-};
